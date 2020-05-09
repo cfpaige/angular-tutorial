fb35cd385abd6551b285942d4876832ee3b912d0
cfpaige <cfpaige@gmail.com>
2020-05-09 17:36:00 -0400

    feature component code complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { KittensComponent } from './kittens/kittens.component';
+import { KittenDetailComponent } from './kitten-detail/kitten-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    KittensComponent
+    KittensComponent,
+    KittenDetailComponent
   ],
   imports: [
     BrowserModule,
